438566a51b2598e7468d7d62aa6e964981a770bd
myyhikali <31601136@stu.zucc.edu.cn>
2019-05-15 15:30:44 +0800

    1.提供了select方法                 2.在service中调用DAO

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,4 +1,8 @@
+import com.zucc.doublefish.news.pojo.Article;
+import com.zucc.doublefish.news.pojo.Column;
 import com.zucc.doublefish.news.pojo.User;
+import com.zucc.doublefish.news.service.ArticleService;
+import com.zucc.doublefish.news.service.ColumnService;
 import com.zucc.doublefish.news.service.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,10 +20,19 @@ public class UserTest {
 
     @Autowired
     private UserService userService;
+    @Autowired
+    private ColumnService columnService;
+    @Autowired
+    private ArticleService articleService;
 
     @Test
     public void test(){
-        User users = userService.findUserByUname("admin");
-        System.out.println(users.getUname());
+//        User users = userService.findUserByUname("admin");
+//        System.out.println(users.getUname());
+
+//        List<Column> list=columnService.findAllColumns();
+//        System.out.println(list);
+        List<Article> articleList=articleService.findAllArticlesByColumnid(1000);
+        System.out.println(articleList);
     }
 }
